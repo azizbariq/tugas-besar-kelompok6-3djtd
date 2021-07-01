7437a9e5ce258bc5162e6c6268391e8b3df19fac
AzizBariqBaharsyah <79688614+AzizBariqBaharsyah@users.noreply.github.com>
2021-07-01 19:53:00 +0700

    make it better

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 // koneksi Mysql
-$servername = "mysql://bc3cd2951e4851:ea7e5e3a@us-cdbr-east-04.cleardb.com/heroku_0d20d3f33aa1507?reconnect=true";
+$servername = "us-cdbr-east-04.cleardb.com/heroku_0d20d3f33aa1507?reconnect=true";
 $username = "bc3cd2951e4851";
 $password = "ea7e5e3a";
 $db = "barang";
